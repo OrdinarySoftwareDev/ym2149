45a794e6a066d1cee05090891d97d94f6a1e31f7
vw.dvw <jaba2137@proton.me>
2026-01-14 00:43:00 +0100

    fix clock

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,84 @@
 #![no_std]
 #![no_main]
 
-use bsp::entry;
 use cortex_m::asm::delay;
+use rp2040_boot2;
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+use hal::entry;
 use defmt_rtt as _;
 use panic_probe as _;
 
-mod ym2149f;
+//mod ym2149f;
 
-use rp_pico::{self as bsp, hal::gpio::PinGroup};
-use bsp::hal as hal;
-use bsp::pac::Peripherals;
+use rp2040_hal as hal;
 
 use hal::{
-    clocks::{ init_clocks_and_plls },
+    clocks::init_clocks_and_plls,
     pac,
     sio::Sio,
+    gpio::{ PinState, PinGroup },
     watchdog::Watchdog,
+    Clock
 };
-
-use hal::gpio::{DynPinId, FunctionSioOutput, Pin, Pins, PullNone};
-use hal::{Clock, clocks::ClocksManager};
 
 use embedded_hal::pwm::SetDutyCycle;
 use embedded_hal::digital::OutputPin;
 
-use crate::ym2149f::YM2149F;
+//use crate::ym2149f::YM2149F;
 
+pub enum Mode {
+    INACTIVE,
+    //READ,
+    WRITE,
+    ADDRESS
+}
 
+pub enum Register {
+    AFreq8bitFinetone,
+    AFreq4bitRoughtone,
+    BFreq8bitFinetone,
+    BFreq4bitRoughtone,
+    CFreq8bitFinetone,
+    CFreq4bitRoughtone,
+    NoiseFreq5bit,
+    IoPortMixerSettings,
+    ALevel,
+    BLevel,
+    CLevel,
+    EFreq8bitFineAdj,
+    EFreq8bitRoughAdj,
+    EShape,
+    DataIoA,
+    DataIoB
+}
+
+fn set_mode(
+    bdir: &mut impl OutputPin,
+    bc1: &mut impl OutputPin,
+    mode: Mode
+){
+    use PinState::{*};
+
+    let arr: [PinState; 3] = match mode {
+        //MODE:: (...) => [BDIR, BC2, BC1],
+        Mode::INACTIVE => [Low, High, Low],
+        //Mode::READ => [false, true, true],
+        Mode::WRITE => [High, High, Low],
+        Mode::ADDRESS => [High, High, High]
+    };
+
+    bdir.set_state(arr[0]).unwrap();
+    //bc2.set_state(arr[1]).unwrap();
+    bc1.set_state(arr[2]).unwrap();
+}
 
 #[entry]
 fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    //let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
@@ -49,7 +95,7 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let pins = bsp::Pins::new(
+    let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
@@ -74,6 +120,7 @@ fn main() -> ! {
         gpio27
     );*/
 
+    // Set up 2MHz clock on GPIO0
     use hal::pwm::{*};
 
     let pwm_slices = Slices::new(pac.PWM, &mut pac.RESETS);
@@ -89,26 +136,57 @@ fn main() -> ! {
     pwm.enable();
 
     let channel = &mut pwm.channel_a;
-    channel.output_to(pins.gpio0);
     channel.set_duty_cycle_fraction(1, 2);
 
-    pins.led.into_push_pull_output().set_high();
+    channel.output_to(pins.gpio0);
 
-    let mut data_bus = rp2040_hal::gpio::PinGroup::new()
+    // turn on onboard led to give any sign of life
+    pins.gpio25.into_push_pull_output().set_high();
+
+    // pins
+    let mut bc1 = pins.gpio9.into_push_pull_output();
+    let mut bdir = pins.gpio10.into_push_pull_output();
+
+    let mut data_bus = PinGroup::new()
         .add_pin(pins.gpio1.into_push_pull_output()) // LSB, DA0
-        .add_pin(pins.gpio2.into_push_pull_output())
-        .add_pin(pins.gpio3.into_push_pull_output())
-        .add_pin(pins.gpio4.into_push_pull_output())
-        .add_pin(pins.gpio5.into_push_pull_output())
-        .add_pin(pins.gpio6.into_push_pull_output())
-        .add_pin(pins.gpio7.into_push_pull_output())
+        .add_pin(pins.gpio2.into_push_pull_output()) // ...
+        .add_pin(pins.gpio3.into_push_pull_output()) // ...
+        .add_pin(pins.gpio4.into_push_pull_output()) // ...
+        .add_pin(pins.gpio5.into_push_pull_output()) // ...
+        .add_pin(pins.gpio6.into_push_pull_output()) // ...
+        .add_pin(pins.gpio7.into_push_pull_output()) // ...
         .add_pin(pins.gpio8.into_push_pull_output()); // MSB, DA8
 
+    data_bus.set_u32(0x00);
 
+    set_mode(&mut bdir, &mut bc1, Mode::INACTIVE);
 
-    loop {
-
+    macro_rules! write_to_bus {
+        ($value:expr) => {
+            data_bus.set_u32(($value * 2) as u32);
+        };
     }
+
+    macro_rules! write_register {
+        ($register:expr, $value:expr) => {
+            set_mode(&mut bdir, &mut bc1, Mode::ADDRESS);
+            write_to_bus!($register);
+            set_mode(&mut bdir, &mut bc1, Mode::INACTIVE);
+            set_mode(&mut bdir, &mut bc1, Mode::WRITE);
+            write_to_bus!($value);
+            set_mode(&mut bdir, &mut bc1, Mode::INACTIVE);
+        };
+    }
+
+    write_register!(7, 0b11000111);
+    write_register!(8, 0b00001111);
+
+    write_register!(0, 210);
+    write_register!(1, 0);
+
+    delay(50_000_000);
+
+    loop {}
 
     // 00000000 write 0x00 to bus
     // 11100000 write to r7 (io)
